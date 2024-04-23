741132f90c889f5078092d8563c53cf18f0f2df9
Kevin Larrabee <139161041+KLarrabee-Arcadia@users.noreply.github.com>
2024-04-23 15:31:42 -0400

    spark/hudi dockerfile and update dbt to use spark

---
+++ dockerfiles/hudi/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Start Spark master and worker
+/${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_NAME --port $SPARK_MASTER_PORT --webui-port 8080 &
+/${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER_NAME:$SPARK_MASTER_PORT --webui-port 8081 &
+wait
