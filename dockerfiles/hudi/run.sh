423c43472d4746f5c17a63d95291408ccf1ff073
Kevin Larrabee <139161041+KLarrabee-Arcadia@users.noreply.github.com>
2024-04-23 16:26:21 -0400

    hah it worked

--- dockerfiles/hudi/run.sh
+++ dockerfiles/hudi/run.sh
@@ -3,4 +3,5 @@
 # Start Spark master and worker
 /${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_NAME --port $SPARK_MASTER_PORT --webui-port 8080 &
 /${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER_NAME:$SPARK_MASTER_PORT --webui-port 8081 &
+/${SPARK_HOME}/sbin/start-thriftserver.sh &
 wait
