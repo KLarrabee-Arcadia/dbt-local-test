0a0192d6a54ff95a1fb379745b9a26e5a8a1cfeb
Kevin Larrabee <139161041+KLarrabee-Arcadia@users.noreply.github.com>
2024-04-24 15:17:03 -0400

    hudi take 2, with derby

---
+++ dockerfiles/hudi/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start Derby server
+#
+# `startNetworkServer` doesn't appear to be able to be sent to the background with `&`
+# so using `nohup` appears to be necessary. The main downside is that this redirects
+# output so docker logs will not show it.
+nohup $DERBY_HOME/bin/startNetworkServer -h 0.0.0.0 &
+
+# Start Thrift server pointing to Derby backend
+$SPARK_HOME/sbin/start-thriftserver.sh \
+--conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+--conf spark.sql.extensions=org.apache.spark.sql.hudi.HoodieSparkSessionExtension \
+--conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.hudi.catalog.HoodieCatalog \
+--conf spark.sql.warehouse.dir=/tmp/hudi/hive/warehouse \
+--hiveconf hive.metastore.warehouse.dir=/tmp/hudi/hive/warehouse \
+--hiveconf hive.metastore.schema.verification=false \
+--hiveconf datanucleus.schema.autoCreateAll=true \
+--hiveconf javax.jdo.option.ConnectionDriverName=org.apache.derby.jdbc.ClientDriver \
+--hiveconf 'javax.jdo.option.ConnectionURL=jdbc:derby://localhost:1527/default;create=true'
+
+sleep infinity
